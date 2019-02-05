8e4d968c97ca4772aaa0087d5d4d32db6267be96
Erik <erik.saarenvirta93@gmail.com>
2019-02-05 11:22:25 -0500

    Update WebScraper.py

--- com/erik/dev/WebScraper.py
+++ com/erik/dev/WebScraper.py
@@ -5,6 +5,7 @@ from BeautifulSoup import BeautifulSoup
 from urllib import urlretrieve
 import re
 import logging
+import pickle
 
 logging.basicConfig()
 logger = logging.getLogger('H&M SCRAPER')
@@ -21,28 +22,63 @@ def removeStringSpecialCharacters(s):
     return stripped
 
 def run():
-    url = 'https://www2.hm.com/en_ca/women/shop-by-product/shirts-and-blouses.html?product-type=ladies_shirtsblouses&sort=stock&image-size=small&image=model&offset=0&page-size=1024'
-    home_path = '/Users/erik.saarenvirta/Documents/Work/Projects/crawler/data/'
-    response = requests.get(url)
-    parsed_html = BeautifulSoup(response.content)
-    for a in parsed_html.body.findAll('a'):
-        if 'productpage' in a['href']:
-            product_url = 'https://www2.hm.com' + a['href']
-            product_code = product_url.split('productpage.')[1].split('.')[0]
-            folder_create_path = home_path + product_code
-            os.system('mkdir -p {}'.format(folder_create_path))
-            print 'Product code is: ', product_code
-            print "Grabbing URL: ", product_url
-            response = requests.get(product_url)
-            parsed_page = BeautifulSoup(response.content)
-            for image in parsed_page.body.findAll('img'):
-                if 'hm.com' in image.get('src'):
-                    image_source = 'https:' + image.get('src')
-                    image_name = removeStringSpecialCharacters(image.get('alt'))
-                    print 'Page Image Source: ', image_source
-                    print 'Getting Image...'
-                    urlretrieve(image_source, "{}/{}.jpg".format(folder_create_path, image_name))
 
+
+    url_dict = {}
+
+    url_dict['blouse'] = 'https://www2.hm.com/en_ca/women/shop-by-product/shirts-and-blouses.html?product-type=ladies_shirtsblouses&sort=stock&image-size=small&image=model&offset=0&page-size=1024'
+    url_dict['top'] = 'https://www2.hm.com/en_ca/women/shop-by-product/tops.html?product-type=ladies_tops&sort=stock&image-size=small&image=model&offset=0&page-size=496'
+    url_dict['dress'] = 'https://www2.hm.com/en_ca/women/shop-by-product/dresses.html?product-type=ladies_dresses&sort=stock&image-size=small&image=model&offset=0&page-size=1024'
+    url_dict['cardigan'] = 'https://www2.hm.com/en_ca/women/shop-by-product/cardigans-and-jumpers.html?product-type=ladies_cardigansjumpers&sort=stock&image-size=small&image=model&offset=0&page-size=1024'
+    home_path = '/Users/erik.saarenvirta/Documents/Work/Projects/crawler/data/'
+    products_saved = []
+    try:
+        with open('products_saved.pickle', 'rb') as handle:
+            products_saved = pickle.load(handle)
+    except Exception as e:
+        logger.error(e)
+        logger.warn('This is probably first time run ... instantiating save array')
+        products_saved = []
+
+    for url_key in url_dict:
+        url = url_dict[url_key]
+        logger.info('Working on products from: ' + url_key)
+        response = requests.get(url)
+        parsed_html = BeautifulSoup(response.content)
+        for a in parsed_html.body.findAll('a'):
+            if 'productpage' in a['href']:
+                product_url = 'https://www2.hm.com' + a['href']
+                if product_url not in products_saved:
+                    product_code = product_url.split('productpage.')[1].split('.')[0]
+                    folder_create_path = home_path + product_code
+                    os.system('mkdir -p {}'.format(folder_create_path))
+                    logger.info('Product code is: ' + product_code)
+                    logger.info('Grabbing URL: ' + product_url)
+                    try:
+                        response = requests.get(product_url)
+                        parsed_page = BeautifulSoup(response.content)
+                        for image in parsed_page.body.findAll('img'):
+                            if 'hm.com' in image.get('src'):
+                                image_source = 'https:' + image.get('src')
+                                image_name = removeStringSpecialCharacters(image.get('alt'))
+                                logger.info('Page Image Source: ' + image_source)
+                                logger.info('Getting Image...')
+                                try:
+                                    urlretrieve(image_source, "{}/{}.jpg".format(folder_create_path, image_name))
+                                    products_saved.append(product_url)
+                                    with open('products_saved.pickle', 'wb') as handle:
+                                        pickle.dump(products_saved, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+                                except Exception as e:
+                                    logger.error('Failed to retrieve image: ' + image_source)
+                                    logger.error(e)
+
+                    except Exception as e:
+                        logger.error('Failed to get the product with url: ' + product_url)
+                        logger.error(e)
+
+                else:
+                    logger.info('Already saved product: ' + product_url)
 
 
 if __name__ == '__main__':
